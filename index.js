ca223133cdb4485f5f8e393be39b10c5b8d914bf
Gunther Feldbaumer <gunther@offworldsim.com>
2020-09-08 13:54:19 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+let threadID = "25277";
+const baseUrl = "https://forums.sim-football.com/";
+const parsedResults = [];
+const url = baseUrl + "showthread.php?tid=" + threadID;
+const team = ["r0tzbua", "OrbitingDeath", "SlurmsMckenzie", "nexus1353", "bj_owns", "TheWoZy", "AirCrou", "Nowordofalie", "Asked Madden", "nutbarspadge", "Oneski", "Regius", "McCransky", "zaynzk", "r0tzbua", "DingusDiddler", "Kai", "Bkfootball", "ReverendOReily", "Official DT", "DunkinDuncan", "Skyfish", "xTri", "PigSnout", "Kyamprac", "hexify"];
+
+console.log(`Scraping of ${url} initiated ...\n`);
+
+const getThreadContent = async (url) => {
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+
+    $(".post_author").each((i, el) => {
+      const user = $(el).children().children().first().text();
+      parsedResults.push(user);
+
+      // parsedResults.push(metadata);
+    });
+
+    const nextPageLink = $(".pagination")
+      .find(".pagination_current")
+      .next("a")
+      .attr("href");
+
+    if (nextPageLink === undefined) {
+      console.log(team.filter( e => !parsedResults.includes(e)) );
+      return false;
+    }
+
+    console.log(`Scraping: ${nextPageLink}`);
+    getThreadContent(baseUrl + nextPageLink);
+  } catch (error) {
+    console.log(team.filter( e => !parsedResults.includes(e)) );
+    console.error(error);
+  }
+};
+
+getThreadContent(url);
